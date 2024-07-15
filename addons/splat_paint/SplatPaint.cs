c3a9b0d19afdf758943473139e56baf854dc0a52
Tshmofen <timohatimafeikq@gmail.com>
2024-07-16 00:32:30 +0300

    Refactor painting logic

--- addons/splat_paint/SplatPaint.cs
+++ addons/splat_paint/SplatPaint.cs
@@ -2,6 +2,7 @@
 using Godot;
 using GroundPainter.addons.splat_paint.UI;
 using GroundPainter.addons.splat_paint.Util;
+using static System.Net.Mime.MediaTypeNames;
 using Image = Godot.Image;
 using Vector4 = Godot.Vector4;
 
@@ -37,7 +38,7 @@ public partial class SplatPaint : MeshInstance3D
         return searchName;
     }
 
-    private static Image PrepareImage()
+    private static Image CreateSplatImage()
     {
         var image = Image.Create(SplatSize, SplatSize, false, Image.Format.Rgba8);
         image.Fill(new Color(1, 0, 0, 0));
@@ -81,6 +82,54 @@ public partial class SplatPaint : MeshInstance3D
         // Vector distance formula + hardcoded adjustments
         var adjustedFactor = 1 - ((x - brushCenter) * (x - brushCenter) + (y - brushCenter) * (y - brushCenter)) / (brushCenter * brushCenter / 2);
         return Mathf.Clamp(adjustedFactor, 0, maxFactor);
+    }
+
+    private ImageTexture GetSplatTexture()
+    {
+        if (!IsCurrentShaderSupportsSplatMap())
+        {
+            Mesh.SurfaceSetMaterial(0, new ShaderMaterial
+            {
+                Shader = SplatPaintPlugin.LoadPluginResource<Shader>(DefaultShaderPath)
+            });
+        }
+
+        var shaderMaterial = (ShaderMaterial)Mesh.SurfaceGetMaterial(0);
+        var splatTexture = shaderMaterial.GetShaderParameter(SplatMapParameter).As<ImageTexture>();
+
+        if (splatTexture == null)
+        {
+            splatTexture = ImageTexture.CreateFromImage(CreateSplatImage());
+            shaderMaterial.SetShaderParameter(SplatMapParameter, splatTexture);
+        }
+
+        return splatTexture;
+    }
+
+    private static void PaintSplatImage(Image image, Vector4 paintMask, float paintSize, float paintForce, int destinationX, int destinationY)
+    {
+        var maxFactor = Mathf.Ease(paintForce / 100, 2.5f);
+        var splatSize = image.GetSize();
+
+        // TODO: Supper ineffective, should be a viewport with shader, though is suitable for me at the moment
+        for (var x = 0; x < paintSize; x++)
+        {
+            for (var y = 0; y < paintSize; y++)
+            {
+                var finalX = destinationX + x;
+                var finalY = destinationY + y;
+
+                if ((finalY < 0 || finalY >= splatSize.Y) || (finalX < 0 || finalX >= splatSize.X))
+                {
+                    continue;
+                }
+
+                var factor = GetBrushFactor(paintSize / 2f, x, y, maxFactor);
+                var currentColor = image.GetPixel(finalX, finalY);
+                var targetColor = BlendSplat(currentColor, paintMask, factor);
+                image.SetPixel(finalX, finalY, targetColor);
+            }
+        }
     }
 
     #endregion
@@ -161,29 +210,11 @@ public partial class SplatPaint : MeshInstance3D
 
     public void SelectorPaint(Vector3 globalPoint, Vector3 normal, Vector4 paintMask, float paintForce, float paintSize)
     {
+        _meshUvTool ??= new MeshUvTool(this);
         if (Mesh == null || Mesh.GetSurfaceCount() == 0)
         {
             return;
         }
-
-        if (!IsCurrentShaderSupportsSplatMap())
-        {
-            Mesh.SurfaceSetMaterial(0, new ShaderMaterial
-            {
-                Shader = SplatPaintPlugin.LoadPluginResource<Shader>(DefaultShaderPath)
-            });
-        }
-
-        var shaderMaterial = (ShaderMaterial)Mesh.SurfaceGetMaterial(0);
-        var splatTexture = shaderMaterial.GetShaderParameter(SplatMapParameter).As<ImageTexture>();
-
-        if (splatTexture == null)
-        {
-            splatTexture = ImageTexture.CreateFromImage(PrepareImage());
-            shaderMaterial.SetShaderParameter(SplatMapParameter, splatTexture);
-        }
-
-        _meshUvTool ??= new MeshUvTool(this);
 
         var uvPosition = _meshUvTool.GetUvCoordinates(globalPoint, normal);
         if (uvPosition == null)
@@ -191,36 +222,14 @@ public partial class SplatPaint : MeshInstance3D
             return;
         }
 
+        var splatTexture = GetSplatTexture();
         var splatImage = splatTexture.GetImage();
-        var splatSize = splatTexture.GetSize();
+        var splatSize = splatImage.GetSize();
 
-        paintSize *= 2.5f;
+        paintSize *= 2.5f; // Adjust to match visible brush
         var destinationX = (int)(uvPosition.Value.X * splatSize.X - paintSize / 2f);
         var destinationY = (int)(uvPosition.Value.Y * splatSize.Y - paintSize / 2f);
-
-        var brushImage = Image.Create((int)paintSize, (int)paintSize, false, Image.Format.Rgba8);
-        brushImage.Fill(VectorToColor(paintMask));
-        var maxFactor = Mathf.Ease(paintForce / 100, 2.5f);
-
-        // TODO: Supper ineffective, should be a viewport with shader
-        for (var x = 0; x < paintSize; x++)
-        {
-            for (var y = 0; y < paintSize; y++)
-            {
-                var finalX = destinationX + x;
-                var finalY = destinationY + y;
-
-                if ((finalY < 0 || finalY >= splatSize.Y) || (finalX < 0 || finalX >= splatSize.X))
-                {
-                    continue;
-                }
-
-                var factor = GetBrushFactor(paintSize / 2f, x, y, maxFactor);
-                var currentColor = splatImage.GetPixel(finalX, finalY);
-                var targetColor = BlendSplat(currentColor, paintMask, factor);
-                splatImage.SetPixel(finalX, finalY, targetColor);
-            }
-        }
+        PaintSplatImage(splatImage, paintMask, paintSize, paintForce, destinationX, destinationY);
 
         splatTexture.SetImage(splatImage);
     }
